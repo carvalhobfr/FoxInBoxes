35ea4b796adcbd1aff1255bd5663d10d86dc2fc4
root <root@DESKTOP-MRC.localdomain>
2020-02-05 14:35:13 +0000

    fixed delete BOx architecture

--- box.js
+++ box.js
@@ -25,10 +25,38 @@ class Box {
     };
     //console.log(this.boxs)
   };
+
+  deleteBox() {
+    let k = 0;
+    for (let obstacle in this.game.obstacles) {
+      for (let box in this.boxes) {
+        let boxY = this.boxes[box].positionY;
+        let boxX = this.boxes[box].positionX;
+        let boxW = 50;
+        let boxH = 50;
+        let obsX = this.game.obstacles[obstacle].positionX;
+        let obsY = this.game.obstacles[obstacle].positionY;
+        let obsH = this.game.obstacles[obstacle].height;
+        let obsW = this.game.obstacles[obstacle].width;
+
+
+        if (boxX + boxW > obsX && boxX < obsX + obsW && boxY + boxH > obsY && boxY < obsY + obsH) {
+          k += 1;
+        };
+      }
+    }
+
+    if (k != 0) {
+      this.boxes.splice(-2, 2);
+      this.game.obstacles.splice(0, 1);
+      // this.game.fox.drawFox();
+      this.game.fox.positionY = this.game.fox.positionY + 2 * GRID_SIZE;
+    }
+  }
 }
 
 
-///box.positionX game.fox.positionX + 10
+///box.positionX game.game.fox.positionX + 10
 const boxImage = new Image()
 // console.log("draw box now!!!")
 boxImage.src = './Images/big-crate.png';
--- game.js
+++ game.js
@@ -7,7 +7,6 @@ class Game {
     this.box = new Box(this);
     this.createObstacles();
     this.gameIsRunning = false;
-    // create an array of boxes 
     this.score = 0;
     this.scoreboard = new Scoreboard(this);
   }
@@ -18,46 +17,10 @@ class Game {
       let obstacle = new Obstacle(this, i * 370);
       this.obstacles.push(obstacle);
       this.positionY = 50 + Math.random() * 500;
-      // console.log(this.obstacles);
-    }
-    // this.score = this.score + 3;
-    // for (let k = 0; k < 100; k++) {
-    //   let obstacle2 = new Obstacle(this, k * 50000);
-    //   this.obstacles.push(obstacle2);
-    //   this.box.positionY = 0 + Math.random() * 50;
-    //   // console.log(this.obstacles);
-    // }
-  }
-  deleteBox() {
-    let k = 0;
-    for (let obstacle in this.obstacles) {
-      for (let box in this.boxs) {
-        let boxY = this.boxs[box].positionY;
-        let boxX = this.boxs[box].positionX;
-        let boxW = 50;
-        let boxH = 50;
-        let obsX = this.obstacles[obstacle].positionX;
-        let obsY = this.obstacles[obstacle].positionY;
-        let obsH = this.obstacles[obstacle].height;
-        let obsW = this.obstacles[obstacle].width;
-        // console.log("check colision", boxY, boxX, boxW, boxH, obsX, obsY, obsH, obsW);
-
-        // if (boxX + boxW > obsX + obsW && boxX < obsX + obsW && boxY + boxH * 2 > obsY && boxY < obsY + obsH / 2) 
-        // console.log("i dont know")
-        if (boxX + boxW > obsX && boxX < obsX + obsW && boxY + boxH > obsY && boxY < obsY + obsH) {
-          k += 1;
-        };
-      }
-    }
-
-    if (k != 0) {
-      this.boxs.splice(-2, 2);
-      this.obstacles.splice(0, 1);
-      this.cleanCanvas();
-      this.fox.drawFox();
-      this.fox.positionY = this.fox.positionY + 2 * GRID_SIZE;
     }
   }
+
+
 
   paint() {
     this.cleanCanvas();
@@ -73,8 +36,7 @@ class Game {
   runLogic() {
     for (let i = 0; i < this.obstacles.length; i++) {
       this.obstacles[i].runLogic();
-      this.cleanCanvas();
-      this.deleteBox();
+      this.box.deleteBox();
     }
   }
 
@@ -84,22 +46,20 @@ class Game {
 
   start() {
     this.gameIsRunning = true;
-    // this.reset();
     this.loop();
-    // this.lose()
 
   }
 
   loop = timestamp => {
-    this.runLogic();
     this.paint();
+    this.runLogic();
     if (this.gameIsRunning === true) {
       window.requestAnimationFrame(this.loop);
     }
   };
 
   reset() {
-    this.boxs = []
+    this.box.boxes = []
     this.obstacles = [];
     this.fox = new Fox(this);
     this.box = new Box(this);
